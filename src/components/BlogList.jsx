9bc0561209dffdd0d63d016fc7c9305348c5139e
Sabas Segovia <sasegovia@ucdavis.edu>
2022-07-23 12:49:19 -0700

    feat: creater improvements-proposal read me

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -3,7 +3,7 @@ import Pagination from "./Pagination";
 import React, { useState, useEffect } from "react";
 import blogs from "../data/blogs.json";
 
-const PAGE_SIZES = [15, 25, 50, 100, 400];
+const PAGE_SIZES = [15, 25, 50, 100];
 
 function BlogList() {
   const [rowsPerPage, setRowsPerPage] = useState(15);
